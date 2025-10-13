6d480d9c9539bb22eff34af3a182010b65c8078f
Maz <77918037+Maz-hub@users.noreply.github.com>
2025-10-13 15:04:54 +0000

    feat(ui): add HeaderSearch component with logo and units dropdown

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { useState, useEffect, useRef } from "react";
+import HeaderSearch from "./components/HeaderSearch";
 
 const App = () => {
   return (
-    <section className="p-4">
-      <h1 className="text-preset-2 font-smooth text-[var(--color-Neutral-0)] text-center px-6 my-12">
-        How's the sky looking today ?
-      </h1>
-    </section>
+    <>
+      <section className="p-4 md:p-6 lg:py-12 lg:px-28">
+        <HeaderSearch />
+        <div>
+          <h1 className="text-preset-2 text-[var(--color-Neutral-0)] text-center px-6 md:px-[140px] my-12 lg:my-16">
+            How's the sky looking today ?
+          </h1>
+        </div>
+      </section>
+    </>
   );
 };
 
--- src/components/HeaderSearch.jsx
+++ src/components/HeaderSearch.jsx
@@ -1,5 +1,30 @@
+import Logo from "../assets/images/logo.svg";
+import DropdownIcon from "../assets/images/icon-dropdown.svg";
+import UnitsIcon from "../assets/images/icon-units.svg";
+
 const HeaderSearch = () => {
-  return <></>;
+  return (
+    <section className="flex justify-between items-center">
+      <div>
+        <img src={Logo} alt="Logo" />
+      </div>
+      <div className="flex items-center px-2.5 py-2 bg-Neutral-800 rounded-md">
+        <img
+          src={UnitsIcon}
+          alt="Gear Icon for Units search"
+          className="w-3.5 h-3.5 "
+        />
+        <p className="text-preset-8 text-[var(--color-Neutral-0)] mx-1.5">
+          Units
+        </p>
+        <img
+          src={DropdownIcon}
+          alt="Dropdown Icon for Units search"
+          className="w-[9px] h-3.5 cursor-pointer"
+        />
+      </div>
+    </section>
+  );
 };
 
 export default HeaderSearch;
