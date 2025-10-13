65b38a4779452a90782ad9c27764e49ab7236c32
Maz <77918037+Maz-hub@users.noreply.github.com>
2025-10-13 15:22:14 +0000

    style(HeaderSearch): enhance responsive design with updated padding and icon sizes

--- src/components/HeaderSearch.jsx
+++ src/components/HeaderSearch.jsx
@@ -8,19 +8,19 @@ const HeaderSearch = () => {
       <div>
         <img src={Logo} alt="Logo" />
       </div>
-      <div className="flex items-center px-2.5 py-2 bg-Neutral-800 rounded-md">
+      <div className="flex items-center px-2.5 md:py-3 py-2 md:px-4 bg-Neutral-800 rounded-md">
         <img
           src={UnitsIcon}
           alt="Gear Icon for Units search"
-          className="w-3.5 h-3.5 "
+          className="w-3.5 md:w-4 h-3.5 md:h-4"
         />
-        <p className="text-preset-8 text-[var(--color-Neutral-0)] mx-1.5">
+        <p className="text-preset-8 text-[var(--color-Neutral-0)] mx-1.5 md:mx-2.5">
           Units
         </p>
         <img
           src={DropdownIcon}
           alt="Dropdown Icon for Units search"
-          className="w-[9px] h-3.5 cursor-pointer"
+          className="w-[9px] md:w-3 h-3.5 md:h-4.5 cursor-pointer"
         />
       </div>
     </section>
