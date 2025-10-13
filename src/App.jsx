f926ce5b6014da73370103429d253630ea592b56
Maz <77918037+Maz-hub@users.noreply.github.com>
2025-10-13 11:01:34 +0000

    style: import Bricolage Grotesque from Google Fonts for headings

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import { useState, useEffect, useRef } from "react";
+
 const App = () => {
   return (
-    <div>
-      <h1 className="text-[var(--color-Neutral-200)]">Weather App</h1>
-
-    </div>
+    <section className="p-4">
+      <h1 className="text-preset-2 font-smooth text-[var(--color-Neutral-0)] text-center px-6 my-12">
+        How's the sky looking today ?
+      </h1>
+    </section>
   );
 };
 
